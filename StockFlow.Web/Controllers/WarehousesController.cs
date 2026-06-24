82dcbaae59a775b7810fb0024d34a619198952eb
Henrique Cesar <henrique.fc18@gmail.com>
2026-06-24 13:29:43 -0300

    Adicionando CRUD de armazens no web.

--- StockFlow.Web/Controllers/WarehousesController.cs
+++ StockFlow.Web/Controllers/WarehousesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StockFlow.Web.Models;
+using System.Net;
 
 namespace StockFlow.Web.Controllers;
 
@@ -18,5 +19,134 @@ public class WarehousesController : Controller
             ?? [];
 
         return View(warehouses);
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var response = await _httpClient.GetAsync($"api/warehouses/{id}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return NotFound();
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var warehouse = await response.Content.ReadFromJsonAsync<WarehouseViewModel>();
+
+        return View(warehouse);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([Bind("Id,Name,Location")] WarehouseViewModel warehouse)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/warehouses", warehouse);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, "Não foi possível cadastrar o armazém.");
+        }
+
+        return View(warehouse);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var response = await _httpClient.GetAsync($"api/warehouses/{id}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return NotFound();
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var warehouse = await response.Content.ReadFromJsonAsync<WarehouseViewModel>();
+
+        return View(warehouse);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Location")] WarehouseViewModel warehouse)
+    {
+        if (id != warehouse.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/warehouses/{id}", warehouse);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound();
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, "Não foi possível atualizar o armazém.");
+        }
+
+        return View(warehouse);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var response = await _httpClient.GetAsync($"api/warehouses/{id}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return NotFound();
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var warehouse = await response.Content.ReadFromJsonAsync<WarehouseViewModel>();
+
+        return View(warehouse);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"api/warehouses/{id}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return NotFound();
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        return RedirectToAction(nameof(Index));
     }
 }
