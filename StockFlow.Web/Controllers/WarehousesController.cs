e3b12607944512163dad33cfed285188907c27be
Henrique Cesar <henrique.fc18@gmail.com>
2026-06-24 10:29:26 -0300

    Criando tela index de armazens.

---
+++ StockFlow.Web/Controllers/WarehousesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockFlow.Web.Models;
+
+namespace StockFlow.Web.Controllers;
+
+public class WarehousesController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public WarehousesController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("StockFlowApi");
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var warehouses = await _httpClient.GetFromJsonAsync<List<WarehouseViewModel>>("api/warehouses")
+            ?? [];
+
+        return View(warehouses);
+    }
+}
---
+++ StockFlow.Web/Models/WarehouseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockFlow.Web.Models;
+
+public class WarehouseViewModel
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nome do Armazém")]
+    public string Name { get; set; } = string.Empty;
+
+    [Display(Name = "Localização")]
+    public string Location { get; set; } = string.Empty;
+}
