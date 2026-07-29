f296308ee72ec61109c876372cc0bb3cb2f60dcc
Henrique Cesar <henrique.fc18@gmail.com>
2026-07-29 12:26:53 -0300

    Adicionando lista de produtos na página home.

--- StockFlow.Web/Controllers/HomeController.cs
+++ StockFlow.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc;
 using StockFlow.Web.Models;
 
@@ -6,9 +7,26 @@ namespace StockFlow.Web.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly HttpClient _httpClient;
+
+    public HomeController(IHttpClientFactory httpClientFactory)
     {
-        return View();
+        _httpClient = httpClientFactory.CreateClient("StockFlowApi");
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var products = await _httpClient.GetFromJsonAsync<List<ProductViewModel>>("api/products")
+            ?? [];
+
+        var topProducts = products
+            .OrderBy(product => product.Id)
+            .Take(3)
+            .ToList();
+
+        ViewBag.ApiBaseUrl = _httpClient.BaseAddress?.ToString().TrimEnd('/');
+
+        return View(topProducts);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
